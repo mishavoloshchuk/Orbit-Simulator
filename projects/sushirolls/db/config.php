043103e72049e0e6152312152d919e4906ccfe48
mishavoloshchuk <voloshchuk123@gmail.com>
2021-08-24 15:15:19 +0300

    My Projects

---
+++ projects/sushirolls/db/config.php
@@ -0,0 +1,11 @@
+<?php 
+	$config = array(
+		'db' => array(
+				'server' => 'localhost',
+				'user' => 'mysql',
+				'password' => 'mysql',
+				'database_name' => 'sushiRolls'
+			)
+	);
+
+require($_SERVER['DOCUMENT_ROOT']."/db/db.php");
