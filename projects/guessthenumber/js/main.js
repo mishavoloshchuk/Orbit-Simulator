eca37db4288bcfa07c88a21c35afc52125f38c64
MishaVoslchuck <voloshchuk123@gmail.com>
2022-03-20 00:32:10 +0200

    Prettier
    Add title. Prettier code. Keyboard arrow buttons support added.

--- projects/guessthenumber/js/main.js
+++ projects/guessthenumber/js/main.js
@@ -1,22 +1,43 @@
-var max = 100;
-let min, mid, attpt;
-min = 1;
-attpt = 1;
+let [min, attpt, max, mid] = [1, 1, 100, 50]; // Init vars
 // Set max number listener
 document.querySelector('#maxNum').addEventListener('input', (e)=>{
-    [min, attpt] = [1, 1];
-    max = +e.target.value;
+    [min, attpt, max] = [1, 1, +e.target.value];
     attempt.innerHTML = "Спроба №: " + attpt + '/' + getAttempts(); 
     showNumb()
 })
-
 // Buttons listener
 document.querySelector('.appInner').addEventListener('click', (e)=>{
     let elem = e.target.name;
+    binarySearch(elem);
+    // Buttons
+    switch (elem) {
+        case 'restart': {
+            [max, min, attpt] = [+maxNum.value, 1, 1];
+            showNumb()
+            attempt.innerHTML = "Спроба №: " + attpt + '/' + getAttempts(); 
+            break;}
+    }  
+    showNumb()
+});
+// Keyboard events listener
+document.addEventListener('keydown', (e)=>{
+    let key = e.keyCode;
+    //console.log(key);
+    switch (key) {
+        case 37:{
+            binarySearch('smaller');
+            break;}
+        case 39:{
+            binarySearch('bigger');
+            break;}
+    }
+})
+// Algoritm of fast search
+function binarySearch(comp){
     findMid()
-    
-    if ((elem == 'smaller' || elem == 'bigger') && attpt != getAttempts() ){
-        switch (elem) {
+    // Bigger and smaller buttons
+    if ((comp == 'smaller' || comp == 'bigger') && attpt != getAttempts() ){
+        switch (comp) {
             case 'smaller': {
                 max = mid - 1;
                 findMid()
@@ -26,55 +47,27 @@ document.querySelector('.appInner').addEventListener('click', (e)=>{
                 findMid()
                 break;}
         }
-
         attpt ++;
-
         attempt.innerHTML = "Спроба №: " + attpt + '/' + getAttempts(); 
     }
-
-    switch (elem) {
-        case 'restart': {
-            [max, min, attpt] = [+maxNum.value, 1, 1];
-            showNumb()
-            attempt.innerHTML = "Спроба №: " + attpt + '/' + getAttempts(); 
-            break;}
-    }
-    
     showNumb()
-});
-
+}
+// Refresh number text
 function showNumb(){
     findMid()
     askNum.innerHTML = mid+'?';
 }
+// Find middle number
 function findMid(){
     let max1 = max<2 ? 2 : max;
     mid = Math.floor((min + max1)/2);
 }
+// Calculate and send attempts throught max number
 function getAttempts(){
     let numb = maxNum.value ? +maxNum.value < 2 ? 2 : +maxNum.value : 2;
     return 1+Math.floor(getBaseLog(2, numb));
 }
+// Get Base Log
 function getBaseLog(x, y) {
     return Math.log(y) / Math.log(x);
-}
-
-function optiFind(numb, arr){
-	var min, max, mid, guess;
-	max = arr.length-1;
-	min = 0;
-
-	while (min <= max){
-		mid = Math.floor((min + max)/2);
-		guess = arr[mid];
-		if (guess == numb){
-			return mid;
-		} else if (guess > numb){
-			max = mid - 1;
-		} else {
-			min = mid + 1;
-		}
-
-	}
-	return NaN;
 }
